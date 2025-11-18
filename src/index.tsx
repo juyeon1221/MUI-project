a45f387f5969d428516af1e9cc38edd13d15bf86
Juyeon Kim <mxmb141210@naver.com>
2025-11-18 14:52:21 +0900

    Remove unused imports for Netlify build

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import Recover from './components/Recover';
-import Confirm from './components/Confirm';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
